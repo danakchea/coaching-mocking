a01d7516601aaba970c1e27d94e9e679a44c9829
Danak <project2k18.dc@gmail.com>
2020-03-13 10:44:49 +1300

    adding mockito and  gitignore

--- src/test/java/ATMServiceTest.java
+++ src/test/java/ATMServiceTest.java
@@ -1,2 +1,4 @@
+import org.junit.jupiter.api.Test;
+
 public class ATMServiceTest {
 }
--- src/test/java/ATMTest.java
+++ src/test/java/ATMTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ATMTest {
+
+    @Mock
+    ATMService atmService;
+
+    @Test
+    public void getBalanceTest()
+    {
+        atmService.getBalance();
+    }
 }
